bbd622a0abee8da790b77f1f7f5e837e70bc32b7
djava387 <risad9020@gmail.com>
2022-11-05 01:20:59 +0000

    Len() checks the number of character

--- milestone_two.py
+++ milestone_two.py
@@ -1,8 +1,7 @@
 guess = input('Enter a single letter')
 print(f'Your letter is {guess}')
 
-if guess==1 and guess.isalpha():
+if len(guess)==1 and guess.isalpha():
     print("Good guess!")
 else:
     print("Oops! That is not a valid input.")
-    
